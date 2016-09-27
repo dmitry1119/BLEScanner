c397418303bf5a419f63c05c39b2b1a9c8de2a70
Dmitry Veretelnikov <dmitryveretelnikov@outlook.com>
2016-09-27 08:12:54 +0300

    updated

---
+++ IoTBLEScannerHeaded/BleScanner.cs
@@ -0,0 +1,110 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Foundation;
+
+namespace IoTBLEScannerHeaded
+{
+    /// <summary>
+    /// Scans advertisements from BLE devices.
+    /// </summary>
+    public class BleScanner
+    {
+        /// <summary>
+        /// An information about an advetisement from a BLE device.
+        /// </summary>
+        public class BleDeviceAdvertisement
+        {
+            /// <summary>
+            /// Advertisement data
+            /// </summary>
+            public BluetoothLEAdvertisement Advertisement { get; private set; }
+            /// <summary>
+            /// Address of the BLE device which sends this advertisement.
+            /// </summary>
+            public ulong Address { get; private set; }
+            /// <summary>
+            /// RSSI value which indicates the signal strength from the BLE device.
+            /// </summary>
+            public int Rssi { get; private set; }
+
+            public float Temperature { get; private set; }
+
+            public BleDeviceAdvertisement(BluetoothLEAdvertisement advertisement, ulong address, int rssi)
+            {
+                this.Advertisement = advertisement;
+                this.Address = address;
+                this.Rssi = rssi;
+                var manufacturerData = this.Advertisement.GetManufacturerDataByCompanyId(0x00fe).SingleOrDefault();
+                if (manufacturerData != null)
+                {
+                    var buffer = new byte[4];
+                    manufacturerData.Data.CopyTo(buffer);
+                    this.Temperature = BitConverter.ToInt32(buffer, 0);
+                }
+                else
+                {
+                    this.Temperature = float.NaN;
+                }
+            }
+        }
+        private readonly BluetoothLEAdvertisementWatcher watcher;
+        private readonly Subject<Unit> resetSubject = new Subject<Unit>();
+        
+        public ReactiveProperty<bool> IsStopped { get; private set; }
+        public ReadOnlyReactiveCollection<BleDeviceAdvertisement> Advertisements { get; private set; }
+
+        /// <summary>
+        /// Start to watch advertisements.
+        /// </summary>
+        public void Start()
+        {
+            this.IsStopped.Value = false;
+            this.watcher.Start();
+        }
+        /// <summary>
+        /// Stop watching advertisements.
+        /// </summary>
+        public void Stop()
+        {
+            this.watcher.Stop();
+        }
+        /// <summary>
+        /// Clear the advertisement collection.
+        /// </summary>
+        public void ClearAdvertisements()
+        {
+            this.resetSubject.OnNext(Unit.Default);
+        }
+
+        public BleScanner()
+        {
+            
+            this.watcher = new BluetoothLEAdvertisementWatcher();
+            
+            this.Advertisements = Observable
+                .FromEvent<TypedEventHandler<BluetoothLEAdvertisementWatcher, BluetoothLEAdvertisementReceivedEventArgs>, BluetoothLEAdvertisementReceivedEventArgs>(
+                    handler => (o, e) => handler(e), handler => this.watcher.Received += handler, handler => this.watcher.Received -= handler)
+                .Select(e => new BleDeviceAdvertisement(e.Advertisement, e.BluetoothAddress, e.RawSignalStrengthInDBm))
+                .Buffer(TimeSpan.FromSeconds(1))                                                                                // Buffer 1[s]
+                .SelectMany(buffer => buffer.GroupBy(advertisement => advertisement.Address).Select(group => group.First()))    // Distinct by device address.
+                .ToReadOnlyReactiveCollection(this.resetSubject);
+
+            this.IsStopped = Observable
+                .FromEvent<TypedEventHandler<BluetoothLEAdvertisementWatcher, BluetoothLEAdvertisementWatcherStoppedEventArgs>, BluetoothLEAdvertisementWatcherStoppedEventArgs>(
+                    handler => (o, e) => handler(e), handler => this.watcher.Stopped += handler, handler => this.watcher.Stopped -= handler)
+                .Select(_ => true)
+                .ToReactiveProperty(true);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,54 @@
+# BLEScanner for Windows10 IoT
+BLE advertisement scanner on Windows 10 IoT Core
+
+![screen](screen.png)
+
+## Requirements
+* Visual Studio 2015 RTM
+* RPi2 with Windows 10 IoT Core
+	- or A Windows 10 build 10586 or above installed device
+* An USB Bluetooth dongle which supports Bluetooth Low Energy
+	- Confirmed with a dongle which uses Broadcomm BCM20702 chip on Win10 IoT Core Public Release
+	- Confirmed with Buffalo BSBT4D09BK dongle on Win10 IoT Core Insider Preview build at Sep.25'2015.
+
+## Install Drivers To BCM20702 USB Dongle
+
+### Warning
+This instruction is now *obsolete* and can not install the driver correctly at Win10 IoT Core Insider Preview.
+You should use a dongle which operates normally without this instruction such as Buffalo BSBT4D09BK.
+
+Skip this section if you are using an USB dongle with other chip or you have already installed drivers to an USB dongle with BCM20702.
+
+DISCLAIMER: I do not assume any responsibilities for any results by following this instruction.
+
+* Check VID and PID of your USB dongle.
+    - Run `devcon status USB\*` in remote PowerShell session.
+* Build "BLEDriverInfPatch" project by VS2015.
+* Run "BLEDriverInfPath" with arguments below.
+    - the computer name of RPi2
+    - VID
+    - PID
+    - e.g. `BLEDriverInfPath win10iot 0A5C 21EC`
+        + computer name = "win10iot"
+        + VID = "0A5C"
+        + PID = "21EC" 
+* Copy generated "BCM20702.inf" to somewhere in RPi2.
+    - e.g. `\\ComputerName\C$\BCM20702.inf`
+* Update drivers with devcon.
+    * Run the command below on RPi2
+        `devcon updateni C:\BCM20702.inf 'DeviceNameOfYourDongle'`
+
+## Run Application From Visual Studio
+* Connect a RPi2 to local network.
+* Open the property page of IoTBLEScannerHeaded in VS2015.
+* Select "ARM" in the Solution platform dropdown listbox.
+* Select *Debug* tab and set "Target device" to "Remote computer."
+* Input the computer name of the RPi2 to the "Remote machine:" text box.
+* Uncheck the "Use authentication" checkbox.
+* Press "F5" or select "Debug - Start debug"
+
+## Usage
+* Press the "Start" button on the upper-right corner in the screen.
+* Turn on a BLE device and make it advertising state.
+* A list of advertisements will be shown.
+
